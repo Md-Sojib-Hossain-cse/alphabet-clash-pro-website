c6e0c2234ccfbaadbf8d8b533a33fe132d3bb47b
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-15 12:02:09 +0600

    play start function added

---
+++ scripts/alpha.js
@@ -0,0 +1,24 @@
+// function startPlay(){
+//     //hide the start window 
+//     //show the play window
+//     const startWindow = document.getElementById('start-window');
+//     startWindow.classList.add('hidden');
+//     const playWindow = document.getElementById('play-window');
+//     playWindow.classList.remove('hidden');
+// }
+
+// continue playing
+function continueGame(){
+    const randomAlphabet = generateRandomAlphabet();
+
+    //display random alphabet
+    const display = document.getElementById('alphabet-display');
+    display.innerText = randomAlphabet;
+}
+
+// startPlay 
+function startPlay(){
+    hideElementById('start-window');
+    showElementById('play-window');
+    continueGame();
+}
---
+++ scripts/utility.js
@@ -0,0 +1,16 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function generateRandomAlphabet(){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const index = Math.round(Math.random()*25);
+    const randomAlphabet = alphabet[index];
+    return randomAlphabet;
+}
